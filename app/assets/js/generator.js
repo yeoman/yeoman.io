f6d5fee8b7ef2e92b741defdbfa338251ec794cf
Eddie Monge <eddie@eddiemonge.com>
2015-09-18 15:53:12 -0700

    use correct address for temp gen page

--- app/assets/js/generator.js
+++ app/assets/js/generator.js
@@ -3,7 +3,7 @@
   'use strict';
 
   function getGeneratorList() {
-    return $.getJSON('https://104.236.186.228');
+    return $.getJSON('http://104.236.186.228');
     // return $.getJSON('https://yeoman-generator-list.herokuapp.com');
   }
 
