4cd30a9c01d17ac73eb640145eda107fa62140cf
Eddie Monge <eddie@eddiemonge.com>
2015-09-18 17:19:39 -0700

    list updates to add downloads and last updated

--- app/assets/js/generator.js
+++ app/assets/js/generator.js
@@ -11,16 +11,46 @@
     return $.getJSON(GETSTATICURL('/blacklist.json'));
   }
 
+  // From http://stackoverflow.com/questions/3177836/
+  // how-to-format-time-since-xxx-e-g-4-minutes-ago-
+  // similar-to-stack-exchange-site
+  function timeSince(date) {
+    date = new Date(date);
+    var seconds = Math.floor((new Date() - date) / 1000);
+    var interval = Math.floor(seconds / 31536000);
+    if (interval > 1) {
+        return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+  }
+
   $(function() {
     $.when(getGeneratorList(), getBlackList())
       .done(function (getGeneratorListArgs, getBlackListArgs) {
         var blocked = getBlackListArgs[0];
         var modules = getGeneratorListArgs[0].filter(function (el) {
-          return el !== null && el.description && blocked.indexOf(el.name) < 0;
+          return el !== null &&
+            el.desc &&
+            blocked.indexOf('generator-' + el.name) < 0;
         }).map(function (el) {
-          el.official = el.ownerWebsite === 'https://github.com/yeoman' ? 'official' : '';
-          el.name = el.name.replace('generator-', '');
-          el.stars = el.stars || 0;
+          el.official = el.owner.site === 'https://github.com/yeoman' ? 'official' : '';
+          el.updated = timeSince(el.updated);
           return el;
         }).sort(function (a, b) {
           return a.stars === b.stars ? 0 : a.stars < b.stars ? 1 : -1;
@@ -35,7 +65,8 @@
         var list = new List('plugins-all', {
           valueNames: [
             'name',
-            'stars'
+            'stars',
+            'dls'
           ]
         });
 
