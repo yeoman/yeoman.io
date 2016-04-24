490a6dfabaf40aac099bc26c87abefa6882aec2a
Oguz KILIC <oguzz.kilic@gmail.com>
2016-04-24 22:05:52 +0300

    sidebar-fix (#635)
    * sidebar-fix
    
    * Update script.js
    
    * Update script.js
    
    * Update main.css
    
    * Update script.js

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -7,6 +7,31 @@
     var $win = $(window);
     var $doc = $(document);
     var $body = $(document.body);
+    var $context = $('.context-nav');
+    var $footer = $('.SiteFooter');
+
+    var fixedElementOffset = $context.offset().top;
+    var footerOffset = $footer.offset().top - 93;
+    var fixedElementHeight = $context.height();
+
+    $win.scroll(function (event) {
+      var y = $(this).scrollTop();
+      if (y < 148) {
+        $context.css('top', '1.3em');
+      } else {
+        $context.css('top', '6em');
+      }
+      if ( y >= fixedElementOffset && y + fixedElementHeight < footerOffset ) {
+        $context.addClass('navbar-fixed').removeClass('navbar-absolute');
+        $context.css({'top':'6em', 'right':'40px'});
+      }
+      else if ( y >= fixedElementOffset && y + fixedElementHeight >= footerOffset ) {
+        var fixEl = (fixedElementHeight / 2) + fixedElementHeight;
+        var newOffset = $footer.offset().top - fixEl;
+        $context.removeClass('navbar-fixed').addClass('navbar-absolute');
+        $context.css({'top':newOffset, 'right':'0'});
+      }
+    });
 
     // Sticky submenu
     (function() {
