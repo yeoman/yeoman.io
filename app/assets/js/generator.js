da63aa07384b4030ac351774cd57c2d4fdd06cd5
Mehdy Dara <mdara@eleven-labs.com>
2016-11-26 22:35:46 +0100

    Use RawGit to fetch list generators stored on github (#696)
    * 3e33c7d Use RawGit to fetch list generators stored on github
    * 40b03db Redirect to npms.io if fail to fetch list
    * 7eaa363 Fix JSHint warning

--- app/assets/js/generator.js
+++ app/assets/js/generator.js
@@ -18,37 +18,59 @@
 
     var tpl = doT.template($('#plugins-all-template').text());
     var pluginsAll = $('#plugins-all');
+    var lastSha = win.Cookies.get('sha');
 
-    $.getJSON('https://storage.googleapis.com/generators.yeoman.io/cache.json')
-    .done(function (plugins) {
-      if (Array.isArray(plugins)) {
-        pluginsAll.html(tpl({
-          modules: plugins.sort(function (a, b) {
-            return a.stars === b.stars ? 0 : a.stars < b.stars ? 1 : -1;
-          })
-        }));
+    function insertErrorMsg() {
+      pluginsAll.html('There was a problem fetching the generator list. Please try again later.<br />Or you can use <a href="https://npms.io/search?q=keywords%3Ayeoman-generator">npms.io</a>.');
+    }
 
-        var list = new win.List('plugins-all', {
-          valueNames: [
-            'name',
-            'owner',
-            'stars',
-            'updated',
-            'downloads',
-            'description'
-          ]
-        });
+    function fetchAndDisplayListGenerators(sha) {
+      $.getJSON('https://cdn.rawgit.com/yeoman/yeoman-generator-list/' + sha + '/cache.json')
+      .done(function (plugins) {
+        if (Array.isArray(plugins)) {
+          pluginsAll.html(tpl({
+            modules: plugins.sort(function (a, b) {
+              return a.stars === b.stars ? 0 : a.stars < b.stars ? 1 : -1;
+            })
+          }));
 
-        if (list.listContainer) {
-          list.on('updated', function () {
-            $('.table thead').toggle(list.matchingItems.length !== 0);
-            $('#search-notfound').toggle(list.matchingItems.length === 0);
+          var list = new win.List('plugins-all', {
+            valueNames: [
+              'name',
+              'owner',
+              'stars',
+              'updated',
+              'downloads',
+              'description'
+            ]
           });
+
+          if (list.listContainer) {
+            list.on('updated', function () {
+              $('.table thead').toggle(list.matchingItems.length !== 0);
+              $('#search-notfound').toggle(list.matchingItems.length === 0);
+            });
+          }
         }
-      }
-      else {
-        pluginsAll.text('There was a problem fetching the generator list. Please try again later');
-      }
-    });
+        else {
+          insertErrorMsg();
+        }
+      })
+      .fail(insertErrorMsg);
+    }
+
+    if (lastSha) {
+      fetchAndDisplayListGenerators(lastSha);
+    } else {
+      $.getJSON('https://api.github.com/repos/yeoman/yeoman-generator-list/commits?sha=cache-generators-list')
+      .done(function (response) {
+        var inThirtyMinutes = new Date(new Date().getTime() + 30 * 60 * 1000);
+        win.Cookies.set('sha', response[0].sha, {
+          expires: inThirtyMinutes
+        });
+        fetchAndDisplayListGenerators(response[0].sha);
+      })
+      .fail(insertErrorMsg);
+    }
   });
 })(window);
